098617ca0ff5a924f5d930b2fb1be95b954e8b0d
Steve Androulakis <steve.androulakis@gmail.com>
2016-09-08 09:58:43 +1000

    refactored vcf parse in app
    using it to display test vcf on page

--- crispr/crispr.py
+++ crispr/crispr.py
@@ -1,8 +1,19 @@
 from flask import Flask, abort, redirect, url_for, \
     render_template
 
+import vcfParse
+
 app = Flask(__name__)
 
 @app.route('/')
 def report():
-    return render_template('report.html')
+
+    
+    vcfFiles = '/home/kirill/projects/MichelleMeilak/firstRun-pilotGenotype/freebiTestRun'
+    bamFiles = '/home/kirill/projects/MichelleMeilak/firstRun-pilotGenotype/bams-arch'
+
+    #print vcfParse.crispr_report_sample_list(vcfFiles)
+    name = '22_freebayes.vcf'
+    vcf = vcfParse.crispr_report_sample_info(vcfFiles, bamFiles, name)
+    
+    return render_template('report.html', name=name, vcf=vcf)
